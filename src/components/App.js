5010910d65ce704b68aed3b07f903ee6f966ca91
Nikk45 <nicksin36@gmail.com>
2023-08-28 19:24:28 +0530

    submit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [warning,setWarning] = useState(false);
+
+
+  const checkData=()=>{
+    if(!user && !password){
+      setWarning(true)
+    }
+    else
+    setWarning(false)
+    setPassword('')
+    setUser('')
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Username:</label>
+        <input type="text" value={user} onChange={(e)=>setUser(e.target.value)}/><br />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        {
+          warning ? <p id="errorMessage">Both username and password are required</p> : ''
+        }
+        <br/>
+        <button onClick={checkData}>Login</button>
     </div>
   )
 }
